75b74518121d354ab707b24b4b07b9cb7ab17dcb
Jacob Babbitt <jacobhbabbitt@gmail.com>
2022-12-12 09:43:51 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+const addNum = (numOne, numTwo) => {
+    sum = numOne + numTwo
+    return sum
+}
+
+const doubleNum = (numThree) => {
+    product = numThree * 2
+    return product
+}
+let integer = addNum(5,7)
+let finalAnswer = doubleNum(integer)
+console.log(finalAnswer)
